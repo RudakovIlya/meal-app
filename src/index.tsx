8c32ca2f19d1dec9831d55726ce8f33b3abb1e2f
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-01-15 20:53:45 +0300

    test deploy

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from "./App";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
 import {store} from "./redux/store";
 
@@ -12,10 +12,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={'/meal-app'} >
+        <HashRouter basename={'/meal-app'}>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
